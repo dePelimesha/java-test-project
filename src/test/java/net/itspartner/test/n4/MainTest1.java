2b71eac568c9e41ea7c22165e8f74e8c2b12c3a4
dePelimesha <36728651+dePelimesha@users.noreply.github.com>
2018-04-08 21:52:32 +0300

    Update MainTest1.java

--- src/test/java/net/itspartner/test/n4/MainTest1.java
+++ src/test/java/net/itspartner/test/n4/MainTest1.java
@@ -1,5 +1,6 @@
 package net.itspartner.test.n4;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -11,15 +12,24 @@ import static org.junit.Assert.*;
 
 @RunWith(Parameterized.class)
 public class MainTest1 {
+
     private int expectedValue;
     private int[] numbers;
+    private static BiggestNumberFinder compare;
+
+    @BeforeClass
+    public static void oneTimeSetUp() {
+        compare = new Main();
+    }
 
     @Parameterized.Parameters
     public static Collection initialize() {
         return Arrays.asList(new Object[][]{
                 {9, new int[]{4, 2, 5, 6, 3, 9}},
                 {5, new int[]{3, 1, 5, 4}},
-                {44, new int[]{2, 8, 11, 3, 44}}
+                {44, new int[]{2, 8, 11, 3, 44},},
+                {4, new int[]{4, 4, 4, 4}},
+                {0, new int[]{}}
         });
     }
 
@@ -30,7 +40,6 @@ public class MainTest1 {
 
     @Test
     public void testFindTheBiggestNumber() {
-        Main compare = new Main();
         assertEquals(expectedValue, compare.findTheBiggestNumber(numbers));
     }
 }
