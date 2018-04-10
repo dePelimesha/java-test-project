e5a11c687be390e1efed0961511df6f5fb8ef11f
dePelimesha <36728651+dePelimesha@users.noreply.github.com>
2018-04-10 22:19:31 +0300

    Update MainTest.java

--- src/test/java/net/itspartner/test/n4/MainTest.java
+++ src/test/java/net/itspartner/test/n4/MainTest.java
@@ -1,32 +1,32 @@
-package net.itspartner.test.n4;
+    package net.itspartner.test.n4;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+    import org.junit.After;
+    import org.junit.Before;
+    import org.junit.Test;
 
-import static org.junit.Assert.*;
+    import static org.junit.Assert.*;
 
-public class MainTest {
+    public class MainTest {
 
-    private Main compare;
-    private int[] firstArray = {4, 2, 5, 6, 3, 9};
-    private int[] secondArray = {3, 1, 5, 4};
-    private int[] thirdArray = {2, 8, 11, 3, 44};
+        private BiggestNumberFinder finder;
+        private int[] firstArray = {4, 2, 5, 6, 3, 9};
+        private int[] secondArray = {3, 1, 5, 4};
+        private int[] thirdArray = {2, 8, 11, 3, 44};
 
-    @Before
-    public void setUp() {
-        compare = new Main();
+        @Before
+        public void setUp() {
+            finder = new Main();
+        }
+
+        @After
+        public void tearDown() {
+            finder = null;
+        }
+
+        @Test
+        public void testFindTheBiggestNumber() {
+            assertEquals(9, finder.findTheBiggestNumber(firstArray));
+            assertEquals(5, finder.findTheBiggestNumber(secondArray));
+            assertEquals(44, finder.findTheBiggestNumber(thirdArray));
+        }
     }
-
-    @After
-    public void tearDown() {
-        compare = null;
-    }
-
-    @Test
-    public void testFindTheBiggestNumber() {
-        assertEquals(9, compare.findTheBiggestNumber(firstArray));
-        assertEquals(5, compare.findTheBiggestNumber(secondArray));
-        assertEquals(44, compare.findTheBiggestNumber(thirdArray));
-    }
-}
